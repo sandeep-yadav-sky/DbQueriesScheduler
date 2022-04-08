2c83b0be6caec2f16eacb2234b84353fb3ec4569
ucandoit-sky <sky02062000@gmail.com>
2022-04-08 18:04:14 +0530

    adds error tests

--- testSlackNotification.py
+++ testSlackNotification.py
@@ -1,11 +1,22 @@
+from distutils.log import error
 import unittest
 from slackNotification import SlackNotification 
+
+def check():
+    x = -1
+    try:
+        if x < 0:
+            raise ValueError
+    except Exception as e:
+        pass
+
 
 class testExecuteQuery(unittest.TestCase):
 
     def test_slackNotification(self):
         self.assertEqual(SlackNotification().post_to_slack_channel("hello","hi"),"success")
-
+    def test_afunction_throws_exception(self):
+        self.assertRaises(TypeError, check())
 
 if __name__ == '__main__':
     unittest.main()
